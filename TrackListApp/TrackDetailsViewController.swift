c756a39427220c7874fae5de9b5ef727ba4ee2e7
IrinaMuravyova <miomir@yandex.ru>
2024-02-06 19:07:25 +0800

    Track details open in special view

---
+++ TrackListApp/TrackDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackListApp
+//
+//  Created by Irina Muravyeva on 06.02.2024.
+//
+
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
